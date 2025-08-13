af41921c72cb5e2db9464536f48c6652d09908ff
shub39 <cptnshubham39@gmail.com>
2025-08-13 08:16:34 +0530

    :construction: added error to carosel
    Signed-off-by: shub39 <cptnshubham39@gmail.com>

--- app/src/main/java/shub39/momentum/project/ui/component/FavDayCard.kt
+++ app/src/main/java/shub39/momentum/project/ui/component/FavDayCard.kt
@@ -5,11 +5,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Warning
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -48,6 +53,15 @@ fun FavDayCard(
                 imageOptions = ImageOptions(
                     contentScale = ContentScale.Crop
                 ),
+                failure = {
+                    Icon(
+                        imageVector = Icons.Rounded.Warning,
+                        contentDescription = "Placeholder",
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .size(70.dp)
+                    )
+                },
                 previewPlaceholder = painterResource(R.drawable.ic_launcher_foreground),
                 modifier = Modifier
                     .clip(MaterialTheme.shapes.medium)
