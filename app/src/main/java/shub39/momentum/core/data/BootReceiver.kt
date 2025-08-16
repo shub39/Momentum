d45a09f1a9b119044c7936a20bdc2794836e052e
shub39 <cptnshubham39@gmail.com>
2025-08-16 12:29:39 +0530

    :construction: boot receiver
    Signed-off-by: shub39 <cptnshubham39@gmail.com>

--- app/src/main/java/shub39/momentum/core/data/BootReceiver.kt
+++ app/src/main/java/shub39/momentum/core/data/BootReceiver.kt
@@ -3,12 +3,32 @@ package shub39.momentum.core.data
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import org.koin.core.annotation.Single
 import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
+import shub39.momentum.core.data.database.ProjectDao
+import shub39.momentum.core.domain.interfaces.AlarmScheduler
 
 @Single
 class BootReceiver : BroadcastReceiver(), KoinComponent {
-    override fun onReceive(context: Context?, intent: Intent?) {
-        TODO("Not yet implemented")
+
+    private val receiverScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
+    override fun onReceive(context: Context, intent: Intent?) {
+        if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
+            val scheduler = get<AlarmScheduler>()
+            val repo = get<ProjectDao>()
+
+            receiverScope.launch {
+                repo.getProjects().first().forEach {
+                    scheduler.schedule(it.toProject())
+                }
+            }
+        }
     }
 }
