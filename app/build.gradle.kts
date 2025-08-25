a40999f1c0708719b0c0dbb7cf012eb31ab978a3
shub39 <cptnshubham39@gmail.com>
2025-08-25 21:08:33 +0530

    :construction: fixed build issues
    Signed-off-by: shub39 <cptnshubham39@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         abi {
             isEnable = true
             reset()
-            include("arm64-v8a", "x86_64")
+            include("arm64-v8a")
             isUniversalApk = false
         }
     }
