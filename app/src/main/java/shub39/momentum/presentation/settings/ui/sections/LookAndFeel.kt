be33bcc0c98c6dacb82f09ded507d391088109a9
shub39 <cptnshubham39@gmail.com>
2026-02-13 11:32:35 +0530

    :sparkles: chore: cleaned up old stuff
    Signed-off-by: shub39 <cptnshubham39@gmail.com>

--- app/src/main/java/shub39/momentum/presentation/settings/ui/sections/LookAndFeel.kt
+++ app/src/main/java/shub39/momentum/presentation/settings/ui/sections/LookAndFeel.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -66,7 +65,6 @@ import shub39.momentum.domain.enums.Fonts
 import shub39.momentum.presentation.settings.SettingsAction
 import shub39.momentum.presentation.settings.SettingsState
 import shub39.momentum.presentation.shared.ColorPickerDialog
-import shub39.momentum.presentation.shared.MomentumDialog
 import shub39.momentum.presentation.shared.MomentumTheme
 import shub39.momentum.presentation.shared.endItemShape
 import shub39.momentum.presentation.shared.leadingItemShape
@@ -85,8 +83,6 @@ fun LookAndFeel(
     modifier: Modifier = Modifier
 ) {
     var colorPickerDialog by remember { mutableStateOf(false) }
-    var fontPickerDialog by remember { mutableStateOf(false) }
-    var themePickerDialog by remember { mutableStateOf(false) }
 
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
@@ -112,226 +108,6 @@ fun LookAndFeel(
             )
         }
     ) { padding ->
-//        LazyColumn(
-//            horizontalAlignment = Alignment.CenterHorizontally,
-//            contentPadding = PaddingValues(
-//                top = paddingValues.calculateTopPadding() + 16.dp,
-//                bottom = paddingValues.calculateBottomPadding() + 16.dp
-//            ),
-//            modifier = Modifier.fillMaxSize()
-//        ) {
-//            // App theme switch
-//            item {
-//                ListItem(
-//                    headlineContent = {
-//                        Text(
-//                            text = stringResource(R.string.select_app_theme)
-//                        )
-//                    },
-//                    supportingContent = {
-//                        Text(
-//                            text = stringResource(state.theme.appTheme.stringRes)
-//                        )
-//                    },
-//                    trailingContent = {
-//                        FilledTonalIconButton(
-//                            onClick = { themePickerDialog = true }
-//                        ) {
-//                            Icon(
-//                                painter = painterResource(R.drawable.edit),
-//                                contentDescription = "Pick"
-//                            )
-//                        }
-//                    }
-//                )
-//            }
-//
-//            if (!isPlusUser) {
-//                item {
-//                    Box(
-//                        contentAlignment = Alignment.Center,
-//                        modifier = Modifier
-//                            .fillMaxWidth()
-//                            .height(100.dp)
-//                            .zigZagBackground()
-//                    ) {
-//                        Spacer(modifier = Modifier.height(16.dp))
-//                        Button(
-//                            onClick = onNavigateToPaywall
-//                        ) {
-//                            Text(
-//                                text = stringResource(R.string.unlock_more_pro)
-//                            )
-//                        }
-//                    }
-//                }
-//            }
-//
-//            // Font Picker
-//            item {
-//                ListItem(
-//                    headlineContent = {
-//                        Text(text = stringResource(R.string.font))
-//                    },
-//                    supportingContent = {
-//                        Text(text = state.theme.font.displayName)
-//                    },
-//                    trailingContent = {
-//                        FilledTonalIconButton(
-//                            onClick = { fontPickerDialog = true },
-//                            enabled = isPlusUser
-//                        ) {
-//                            Icon(
-//                                painter = painterResource(R.drawable.font),
-//                                contentDescription = "Pick Font"
-//                            )
-//                        }
-//                    }
-//                )
-//            }
-//
-//            // Amoled variant toggle
-//            item {
-//                ListItem(
-//                    headlineContent = {
-//                        Text(
-//                            text = stringResource(R.string.amoled)
-//                        )
-//                    },
-//                    supportingContent = {
-//                        Text(
-//                            text = stringResource(R.string.amoled_desc)
-//                        )
-//                    },
-//                    trailingContent = {
-//                        Switch(
-//                            checked = state.theme.isAmoled,
-//                            enabled = isPlusUser,
-//                            onCheckedChange = {
-//                                onAction(SettingsAction.OnAmoledSwitch(it))
-//                            }
-//                        )
-//                    }
-//                )
-//            }
-//
-//            // Material you toggle
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-//                item {
-//                    ListItem(
-//                        headlineContent = {
-//                            Text(
-//                                text = stringResource(R.string.material_theme)
-//                            )
-//                        },
-//                        supportingContent = {
-//                            Text(
-//                                text = stringResource(R.string.material_theme_desc)
-//                            )
-//                        },
-//                        trailingContent = {
-//                            Switch(
-//                                checked = state.theme.isMaterialYou,
-//                                enabled = isPlusUser,
-//                                onCheckedChange = {
-//                                    onAction(SettingsAction.OnMaterialThemeToggle(it))
-//                                }
-//                            )
-//                        }
-//                    )
-//                }
-//            }
-//
-//            // Seed color picker
-//            item {
-//                AnimatedVisibility(
-//                    visible = !state.theme.isMaterialYou,
-//                    modifier = Modifier.fillMaxWidth()
-//                ) {
-//                    ListItem(
-//                        headlineContent = {
-//                            Text(
-//                                text = stringResource(R.string.seed_color)
-//                            )
-//                        },
-//                        supportingContent = {
-//                            Text(
-//                                text = stringResource(R.string.seed_color_desc)
-//                            )
-//                        },
-//                        trailingContent = {
-//                            IconButton(
-//                                onClick = { colorPickerDialog = true },
-//                                colors = IconButtonDefaults.iconButtonColors(
-//                                    containerColor = state.theme.seedColor,
-//                                    contentColor = contentColorFor(state.theme.seedColor)
-//                                ),
-//                                enabled = !state.theme.isMaterialYou && isPlusUser
-//                            ) {
-//                                Icon(
-//                                    painter = painterResource(R.drawable.edit),
-//                                    contentDescription = "Select Color"
-//                                )
-//                            }
-//                        }
-//                    )
-//                }
-//            }
-//
-//            // palette styles
-//            item {
-//                Column {
-//                    ListItem(
-//                        headlineContent = {
-//                            Text(
-//                                text = stringResource(R.string.palette_style)
-//                            )
-//                        }
-//                    )
-//
-//                    LazyRow(
-//                        contentPadding = PaddingValues(horizontal = 16.dp),
-//                        horizontalArrangement = Arrangement.spacedBy(8.dp),
-//                    ) {
-//                        items(PaletteStyle.entries.toList(), key = { it.name }) { style ->
-//                            val scheme = rememberDynamicColorScheme(
-//                                primary = if (state.theme.isMaterialYou && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-//                                    colorResource(system_accent1_200)
-//                                } else {
-//                                    state.theme.seedColor
-//                                },
-//                                isDark = when (state.theme.appTheme) {
-//                                    AppTheme.SYSTEM -> isSystemInDarkTheme()
-//                                    AppTheme.LIGHT -> false
-//                                    AppTheme.DARK -> true
-//                                },
-//                                isAmoled = state.theme.isAmoled,
-//                                style = style
-//                            )
-//
-//                            SelectableMiniPalette(
-//                                selected = state.theme.paletteStyle == style,
-//                                onClick = {
-//                                    onAction(SettingsAction.OnPaletteChange(style = style))
-//                                },
-//                                contentDescription = { style.name },
-//                                enabled = isPlusUser,
-//                                accents = listOf(
-//                                    TonalPalette.from(scheme.primary),
-//                                    TonalPalette.from(scheme.tertiary),
-//                                    TonalPalette.from(scheme.secondary)
-//                                )
-//                            )
-//                        }
-//                    }
-//                }
-//            }
-//
-//            item {
-//                Spacer(modifier = Modifier.padding(60.dp))
-//            }
-//        }
-
         LazyColumn(
             modifier = Modifier.fillMaxSize(),
             contentPadding = PaddingValues(
@@ -637,93 +413,6 @@ fun LookAndFeel(
             onSelect = { onAction(SettingsAction.OnSeedColorChange(it)) },
             onDismiss = { colorPickerDialog = false }
         )
-    }
-
-    // theme picker
-    if (themePickerDialog) {
-        MomentumDialog(
-            onDismissRequest = { themePickerDialog = false }
-        ) {
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                Icon(
-                    painter = painterResource(R.drawable.light_mode),
-                    contentDescription = "Select App Theme",
-                    modifier = Modifier.size(32.dp)
-                )
-
-                Text(
-                    text = stringResource(R.string.select_app_theme),
-                    style = MaterialTheme.typography.titleLarge
-                )
-
-                FlowRow(
-                    horizontalArrangement = Arrangement.spacedBy(4.dp),
-                    verticalArrangement = Arrangement.spacedBy(2.dp)
-                ) {
-                    AppTheme.entries.forEach { appTheme ->
-                        ToggleButton(
-                            checked = state.theme.appTheme == appTheme,
-                            onCheckedChange = {
-                                onAction(SettingsAction.OnThemeSwitch(appTheme))
-                                themePickerDialog = false
-                            }
-                        ) {
-                            Text(text = stringResource(appTheme.stringRes))
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    // Font picker
-    if (fontPickerDialog) {
-        MomentumDialog(
-            onDismissRequest = { fontPickerDialog = false }
-        ) {
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                Icon(
-                    painter = painterResource(R.drawable.font),
-                    contentDescription = "Select App Theme",
-                    modifier = Modifier.size(32.dp)
-                )
-
-                Text(
-                    text = stringResource(R.string.font),
-                    style = MaterialTheme.typography.titleLarge
-                )
-
-                FlowRow(
-                    horizontalArrangement = Arrangement.spacedBy(4.dp),
-                    verticalArrangement = Arrangement.spacedBy(2.dp)
-                ) {
-                    Fonts.entries.forEach { font ->
-                        ToggleButton(
-                            checked = state.theme.font == font,
-                            onCheckedChange = {
-                                onAction(SettingsAction.OnFontChange(font))
-                                fontPickerDialog = false
-                            }
-                        ) {
-                            Text(
-                                text = font.displayName,
-                                fontFamily = FontFamily(Font(font.fontRes))
-                            )
-                        }
-                    }
-                }
-            }
-        }
     }
 }
 
