f859a998ed61343946e4e0c36e809eb368b2a235
shub39 <cptnshubham39@gmail.com>
2026-05-10 16:00:36 +0530

    :wrench: fixed migration errors
    Signed-off-by: shub39 <cptnshubham39@gmail.com>

--- app/src/main/java/shub39/momentum/data/database/ProjectDatabase.kt
+++ app/src/main/java/shub39/momentum/data/database/ProjectDatabase.kt
@@ -25,7 +25,10 @@ import androidx.room3.TypeConverters
     entities = [ProjectEntity::class, DayEntity::class, MontageOptionsEntity::class],
     version = ProjectDatabase.SCHEMA_VERSION,
     exportSchema = true,
-    autoMigrations = [AutoMigration(from = 1, to = 2)],
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2),
+        AutoMigration(from = 2, to = 3),
+    ],
 )
 @TypeConverters(Converters::class)
 abstract class ProjectDatabase : RoomDatabase() {
